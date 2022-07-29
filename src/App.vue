4c0cfc30365f44925c83282b656ac2b680e381ea
coderzy <1114117852@qq.com>
2022-07-29 22:53:41 +0800

    test(main): test commitlint

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,14 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 });
 </script>
 
